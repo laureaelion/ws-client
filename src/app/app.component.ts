8fe43414102c032d01b882e977b8e89f2f834751
Laure <laure37500photmail.com>
2018-09-21 09:13:07 +0200

    chat ok sauf login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+
 import { Component } from '@angular/core';
+import { WebSocketSubject } from 'rxjs/webSocket';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ws-client';
+
+ 
+
+  /**
+   * Instance d'un observable Websocket
+   */
+  private _socket: WebSocketSubject<any>;
+
+  /**
+   * Tableau pour recevoir les messages du serveur
+   */
+  public serverMessages: any[];
+
+  public userInput: String = '';
+
+  constructor() {
+    
+    console.log('connexion client WebSocket');
+
+    this._socket = new WebSocketSubject('ws:127.0.0.1:8999');
+
+    //Initialise le tableau des messages
+    this.serverMessages = [];
+
+    //juste pour tester la communication sortante
+    //this._send();
+
+    //Souscription aux message provenant du serveur
+    this._socket
+      .subscribe( (message) => {
+        console.log(`Le serveur envoie : ` + JSON.stringify(message));
+        this.serverMessages.push(message);
+      },
+      (err) => console.error('Erreur levée : ' + JSON.stringify(err)),
+      ()=> console.warn('completed')
+      );
+    
+    }
+  
+ /** private _send(): void {
+    console.log('envoie un nouveau message vers le serveur : ' + this.userInput);
+    this._socket.next('blabla');
+  }
+  */
+
+  public envoyer(): void {
+    console.log('Envoie la saisie utilisateur vers le serveur : ' + this.userInput);
+    this._socket.next(this.userInput);
+    this.userInput='';
+  }
 }
